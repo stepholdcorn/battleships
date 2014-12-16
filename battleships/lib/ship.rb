b0cbac0f31bcb2e98d65c7dc3efc711dac2f6930
Kieran Goodacre <kierangoodacre@Kierans-MacBook-Pro-2.local>
2014-12-16 17:23:06 +0000

    deleted greay text

--- battleships/lib/ship.rb
+++ battleships/lib/ship.rb
@@ -3,21 +3,8 @@ class Ship
 attr_reader :size
 
 def initialize(size)
-	# @placed = false
 	@size = size
 end
-
-# def on_grid?
-# 	@placed = true
-# end
-
-# def position!
-# 	@placed = true
-# end
-
-# def sink!
-# 	@placed = false
-# end
 
 def self.submarine
 	new(3)
