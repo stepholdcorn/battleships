e87b910614f4e5fb2fef545d3b454ac8cdebf918
stepholdcorn <stephieoldcorn@gmail.com>
2014-12-16 17:28:49 +0000

    Adding the different types of boats

--- battleships/lib/ship.rb
+++ battleships/lib/ship.rb
@@ -10,5 +10,21 @@ def self.submarine
 	new(3)
 end
 
+def self.aircraft_carrier
+	new(5)
+end
+
+def self.battleship
+	new(4)
+end
+
+def self.destroyer
+	new(3)
+end
+
+def self.patrol_boat
+	new(2)
+end
+
 
 end
--- battleships/spec/ship_spec.rb
+++ battleships/spec/ship_spec.rb
@@ -8,4 +8,20 @@ describe Ship do
     expect(Ship.submarine.size).to eq(3)
   end
 
+  it 'should be able to create an aircraft carrier' do
+    expect(Ship.aircraft_carrier.size).to eq(5)
+  end
+
+  it 'should be able to create a battleship' do
+    expect(Ship.battleship.size).to eq(4)
+  end
+
+  it 'should be able to create a destroyer' do
+    expect(Ship.destroyer.size).to eq(3)
+  end
+
+  it 'should be able to create a patrol boat' do
+    expect(Ship.patrol_boat.size).to eq(2)
+  end
+
 end
